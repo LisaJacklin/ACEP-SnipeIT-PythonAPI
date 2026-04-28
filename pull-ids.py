0b051bcdb55d0a402c199c2b2a99db0e37a7d6a5
LisaJacklin <lmjacklin@alaska.edu>
2026-04-28 13:19:48 -0800

    add in custom fields db names

--- pull-ids.py
+++ pull-ids.py
@@ -48,6 +48,42 @@ def get_reference_list(endpoint, api_url, header):
         print(f"An error occured: {e}")
         return []
 
+#attempt to pull in the custom fields created
+def get_fieldset_mapping(fieldset_name, api_url, header):
+    field_map = {}
+
+    try: 
+        response = requests.get(f"{api_url}/fieldsets/", headers=header)
+        if response.status_code != 200:
+            print("Failed to pull custom fieldsets")
+            return {}
+
+        target_id = None
+        for fs in response.json().get('rows', []):
+            if fs.get('name') == fieldset_name:
+                target_id = fs.get('id')
+                break
+
+        if not target_id:
+            print(f"Could not find a fieldset named '{filedset_name}")
+            return {}
+
+        # get fields assigned to the fieldset
+        fields_resp = requests.get(f"{api_url}/fildsets/{target_id}/fields", headers=header)
+        if response.status_code == 200:
+            for field in fields_resp.json().get('rows', []): 
+                label = field.get('name')
+                db_name = field.get('db_column')
+
+                if label and db_name:
+                    mapping[label] = db_name
+
+        return field_map
+
+    except Exception as e:
+        print(f"Error occured while mapping: {e}")
+        return {}
+
 # simplify main
 def main():
     #load in the config file
@@ -71,6 +107,25 @@ def main():
         print(f"Could not find config or cred file: {e}")
         return
     
+    # hardcoded custom fields db_name...
+    #haven't had luck yet with being able to query them without getting Null
+    system_specs = {
+        "RAM_GB":"_snipeit_ram_gb_2",
+        "Storage_GB":"_snipeit_storage_gb_3",
+        "Hostname":"_snipeit_hostname_4", 
+        "Operating System":"_snipeit_operating_system_5", 
+        "CPU":"_snipeit_cpu_10", 
+        "GPU":"_snipeit_gpu_11", 
+        "Model Number":"_snipeit_model_number_30",
+        "UUID":"_snipeit_uuid_31"
+
+        #other fields not needed off the bat...
+        # "MAC Address":"_snipeit_mac_address_1", 
+        # "UA Domain":"_snipeit_ua_domain_6", 
+        # "ACEP OU":"_snipeit_acep_ou_7", 
+        # "Login":"_snipeit_login_8"
+    }
+
     data_to_store = {
         "manufacturers": get_reference_list("manufacturers", api_url, header),
         "categories": get_reference_list("categories", api_url, header),
@@ -84,8 +139,11 @@ def main():
         "companies": get_reference_list("companies", api_url, header),
         "departments": get_reference_list("departments", api_url, header),
         "suppliers": get_reference_list("suppliers", api_url, header),
-        "depreciations": get_reference_list("depreciations", api_url, header)
+        "depreciations": get_reference_list("depreciations", api_url, header),
+        "system_specs": system_specs
     }
+
+
 
     #store output to json file
     try:
